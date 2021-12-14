88697108371959462ed86ca9659b8efdf6e6fca6
Subha Das <rohan6056@hotmail.com>
2021-12-13 19:26:35 -0800

    Fixed router

--- backend/index.go
+++ backend/index.go
@@ -4,11 +4,21 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
 )
 
 var working_dir, _ = os.Getwd()
 var REACT_DIR = filepath.Join(working_dir, "frontend/build/")
 
-func (s *Server) HandleIndex() http.Handler {
-	return http.FileServer(http.Dir(REACT_DIR))
+func (s *Server) HandleIndex() http.HandlerFunc {
+	fileServer := http.FileServer(http.Dir(REACT_DIR))
+	fileMatcher := regexp.MustCompile(`\.[a-zA-Z]*$`)
+	return func(w http.ResponseWriter, r *http.Request) {
+		if !fileMatcher.MatchString(r.URL.Path) {
+			http.ServeFile(w, r, REACT_DIR+"/index.html")
+		} else {
+			fileServer.ServeHTTP(w, r)
+		}
+	}
+
 }
